5d398c53e4b0fc26f07c91c464474d5c3ff8c813
Diego Pasti <diegopasti@gmail.com>
2017-05-03 15:21:55 -0300

    Criação das regras de validação para a pagina de registro do contratante, adequações do template base para as paginas de login/register, funções javascripts genéricas e especificas para a criação de um novo usuário.

---
+++ static/modules/core/base.js
@@ -0,0 +1,50 @@
+/**
+ * Created by diego on 03/05/2017.
+ */
+
+function notify(type,title,description){
+  new PNotify({
+    title: title,
+    text: description,
+    type: type,
+    styling: 'bootstrap3'
+  });
+  return (type=='error' ? false : true);
+}
+
+function info_notify(title,description){
+  return notify("info",title,description);
+}
+
+function success_notify(title,description){
+  return notify("success",title,description);
+}
+
+function error_notify(id,title,description){
+  document.getElementById(id).focus();
+  return notify("error",title,description);
+}
+
+function warning_notify(id,titulo,descricao){
+  document.getElementById(id).focus();
+  return notify("warning",titulo,descricao);
+}
+
+function contains_minimal_size(id,size){
+  return (document.getElementById(id).value.length>=size);
+}
+
+function contains_numeric(id){
+  var numbers = /\d+/g;
+  return (document.getElementById(id).value.match(numbers) ? true : false);
+}
+
+function contains_alpha(id) {
+  var letters = /^[a-zA-Z]+$/;
+  return (document.getElementById(id).value.match(letters) ? true : false);
+}
+
+function is_empty(id){
+  return (document.getElementById(id).value.length == 0 ? !error_notify(id,"Campo Obrigatório","Por favor preencha o campo.") : false)
+}
+
---
+++ static/modules/usuario/register.js
@@ -0,0 +1,46 @@
+function criar_usuario_contratante(){
+  NProgress.start();
+  if (validar_email() && comparar_senhas()){
+    success_notify("Usuário Cadastrado","Você receberá um email em instantes.")
+    NProgress.done();
+    return true;
+  }
+  else{
+    NProgress.done();
+    return false;
+  }
+}
+
+function comparar_senhas(){
+  var senha = document.getElementById("senha").value;
+  var re_senha = document.getElementById("re_senha").value;
+  if(senha === re_senha) {
+    var senha_vazia = is_empty('senha');
+    if(!senha_vazia && contains_alpha("senha") && contains_numeric("senha") && contains_minimal_size("senha",8)){
+      return true;
+    }
+    else{
+      if(!senha_vazia){
+        return error_notify("senha","Senha insegura","Informe uma senha com ao menos 8 caracteres contendo letras e numeros.")
+      }
+    }
+  }
+  else{
+    return error_notify("senha","Senhas não conferem","Verifique as senhas informadas.")
+  }
+}
+
+function validar_email() {
+  var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  if(!is_empty('email')){
+    if(filter.test(document.getElementById("email").value)){
+      return true;
+    }
+    else{
+      return error_notify("email","Email Inválido","Verifique se o email foi digitado corretamente.")
+    }
+  }
+  else{
+    return false;
+  }
+}
