154657bfa04ec17ddce7f97f56b9938abd1d25ef
CleitonLC <cleiton.creton@gmail.com>
2017-11-15 12:44:18 -0200

    Ajustes no código e adição da função para retornar os links dos arquivos listados.

--- libs/backup/backup.py
+++ libs/backup/backup.py
@@ -1,10 +1,11 @@
-from libs.backup.dbdropbox import DropBoxStorage
 from django.core.management import call_command
 import datetime
 import dropbox
 import django
 import re
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistemaweb.settings")
 
 from sistemaweb.settings import DBBACKUP_STORAGE_OPTIONS, DROPBOX_ROOT_PATH, DROPBOX_OAUTH2_TOKEN
 
@@ -23,21 +24,48 @@ class BackupManager:
         backup_path = self.upload()
         self.clear_temp_file()
         backup_duration = datetime.datetime.now() - start_timing_backup
-        print("Processado em ",backup_duration.total_seconds()+" segundos. Arquivo disponivel em "+backup_path)
+        print("Backup gerado em",backup_duration.total_seconds(),"segundos")
+        print("Arquivo disponivel em "+backup_path)
+        return backup_path
 
     def restore_backup(self):
+        start_timing_backup = datetime.datetime.now()
         list_files = self.dropbox.files_list_folder(DROPBOX_ROOT_PATH)
-        most_recent_backup = self.download(list_files.entries[-1].path_display)  # self.list_files_root_path(self.dt)
+        most_recent_backup = self.download(list_files.entries[-1].path_display)
         django.setup()
         call_command('dbrestore', '-v','0', '-i', 'temp.dump.gz', '-z', '-q','--noinput')
         self.clear_temp_file()
+        backup_duration = datetime.datetime.now() - start_timing_backup
+        print("Backup Restaurado em", backup_duration.total_seconds(), "segundos")
+        return True
 
     def list_backup(self):
-        return DropBoxStorage().list_files_all()
+        print('\n')
+        self.dt = self.dropbox.files_list_folder(DROPBOX_ROOT_PATH)
+        self.data = []
+        for entry in self.dt.entries:
+            data = {}
+            path_name = entry.path_lower
+            link = self.dropbox.sharing_create_shared_link(path_name)  # Mesmo estando obsoleto,esse é o único modo de retornar o link de arquivos já compartilhados...
+            url = link.url
+            dl_url = re.sub(r"\?dl\=0", "?dl=1", url)
+            data['backup_link'] = dl_url
+            data['client_modified'] = entry.client_modified
+            data['size'] = str(entry.size)+" bytes"
+            modified = entry.client_modified
+            time = datetime.timedelta(hours=2)
+            hora = datetime.datetime.strptime(str(modified), '%Y-%m-%d %H:%M:%S')
+            now = hora - time
+            size = entry.size
+            size = str(size) + " bytes"
+            display = entry.name
+            print(display, now , size, '\n'+dl_url)
+            self.data.append(data)
+        return self.data
 
     def download(self,file):
         self.file = (file)
-        file_name = self.file.replace('/backup/', '')
+        self.file.replace('/backup/', '')
         try:
             metadata, res = self.dropbox.files_download(self.file)
         except Exception as erro:
@@ -47,11 +75,6 @@ class BackupManager:
         f = open(final_path, "wb")
         f.write(res.content)
         f.close()
-        #if '.zip' in final_path or '.gz' in final_path:
-        #    print('Arquivo compactado...efetuando descompressão de dados.')
-        #    self.uncompress_file(final_path)
-        #    new_basename = os.path.basename(final_path).replace('.gz', '')
-        #    return new_basename
         return final_path
 
     def upload(self):
