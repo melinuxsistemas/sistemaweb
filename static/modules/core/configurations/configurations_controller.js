3c0a3184a1cd7185894949eac6e24d13610448c1
Lucas Gianordoli <gianordolilucas@gmail.com>
2017-11-16 18:49:19 -0200

    Template configurations Backup

--- static/modules/core/configurations/configurations_controller.js
+++ static/modules/core/configurations/configurations_controller.js
@@ -1,7 +1,7 @@
 /**
  * Created by diego on 14/11/2017 - 14:42.
  */
-var application = angular.module('modules.configurations', ['angularUtils.directives.dirPagination']);
+var application = angular.module('modules.configurations', ['angularUtils.directives.dirPagination','filters']);
 application.controller('configurations_controller', function ($scope) {
 
 	$scope.backups = null;
@@ -15,17 +15,17 @@ application.controller('configurations_controller', function ($scope) {
       success: function (data) {
         $scope.backups = JSON.parse(data).object;
         $("#loading_tbody").fadeOut();
-        $scope.$apply();
         $scope.loaded_backups = true;
         $scope.$apply();
+
       },
 
-      failure: function (data) {
+      failure: function () {
         $scope.loaded_backups = true;
         alert("Não foi possivel carregar a lista")
-      },
+      }
     })
-	}
+	};
 
 	$scope.create_backup = function () {
 		NProgress.start();
@@ -37,21 +37,50 @@ application.controller('configurations_controller', function ($scope) {
       success: function (data) {
       	var response = JSON.parse(data);
       	var item = response.object;
-      	var result = response.result
-      	if(result=true){
+      	if(response.result){
       		$scope.backups.splice(0, 0, item);
 					$scope.$apply();
       	}
-      	register_action(start_request, status)
+      	register_action(start_request, status);
       	NProgress.done();
       },
 
-      failure: function (data) {
-      	alert("Não foi possivel carregar a lista")
+      failure: function () {
+      	alert("Não foi possivel carregar a lista");
         $scope.loaded_backups = true;
-        register_action(start_request, status)
+        register_action(start_request, status);
       	NProgress.done();
-      },
+      }
     })
 	}
 });
+
+angular.module('filters', [])
+	.filter('Filesize', function () {
+		return function (size) {
+			if (isNaN(size))
+				size = 0;
+
+			if (size < 1024)
+				return size + ' Bytes';
+
+			size /= 1024;
+
+			if (size < 1024)
+				return size.toFixed(2) + ' Kb';
+
+			size /= 1024;
+
+			if (size < 1024)
+				return size.toFixed(2) + ' Mb';
+
+			size /= 1024;
+
+			if (size < 1024)
+				return size.toFixed(2) + ' Gb';
+
+			size /= 1024;
+
+			return size.toFixed(2) + ' Tb';
+		};
+	});
